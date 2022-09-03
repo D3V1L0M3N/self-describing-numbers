0ad3e916e012dec92593b113ef932ff9c205dc18
D3V1L0M3N <60878177+D3V1L0M3N@users.noreply.github.com>
2022-09-03 19:31:10 +0930

    utilize loop labels

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// how many numbers would you like to test?
+const numbers = 1_000_000;
+// which number should we start at
+const startAt = 0;
+
+
 function isEven(n) {
   return n % 2 == 0;
 }
@@ -24,13 +30,13 @@ function countOccurences(numberToLookFor, numberToLookIn) {
 
 function main() {
   console.log('Starting number crunch!');
+  const startTime = Date.now();
 
   var results = [];
 
-  for (var i = 0; i < 500000000; i++) {
-    // example number: 14233221
+  testNumbers:
+  for (var i = 0; i < numbers; i++) {
     var number = i;
-    var isValid = true;
   
     // check number has even number of digits
     if (!isEven(number.toString().length)) { continue }
@@ -44,25 +50,22 @@ function main() {
       // example: ['14'] - there should be x1 4 in our number
       // lets check each couplet to see if they're telling the truth
       if (parseInt(couplets[x].charAt(0)) !== countOccurences(couplets[x].charAt(1), number)) {
-        isValid = false;
-        break;
+        continue testNumbers;
       }
   
       // make sure each quantity indicator gets mentioned/tested
       if (!couplets.map(e => e.charAt(1)).includes(couplets[x].charAt(0))) {
-        isValid = false;
-        break;
+        continue testNumbers;
       }
     }
+
     // our number passed the tests!
-    if (isValid) {
-      results.push(number);
-    }
+    results.push(number);
   }
 
-  console.log('Finished!');
-  console.log(results.join('\n'));
-  console.log(results.length);
+  console.log(`Done - took ${Date.now() - startTime}ms`);
+  console.log(results);
 }
+
 
 main();
